d5851d1ed42de778cb2f37763a7aca975bef860b
Rohit Wadi <rohitwadi1@gmail.com>
2024-02-29 16:29:58 +0000

    updated code

--- food_order_project/accountApp/admin.py
+++ food_order_project/accountApp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from accountApp.models import *
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email','username','is_admin','is_staff','is_active']
+    list_filter = ['email', 'username']
+    search_fields = ['email','username']
+
+admin.site.register(User,UserAdmin)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'gender','phone','birthDate','image']
+admin.site.register(Profile,ProfileAdmin)
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['user', 'area','houseNo','roadNo','zipCode']
+
+admin.site.register(Address,AddressAdmin)
