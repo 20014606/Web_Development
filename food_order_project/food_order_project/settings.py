5788ddb92f8c946a3b290f70a5862267bdc5bdc9
Rohit Wadi <rohitwadi1@gmail.com>
2024-03-09 21:21:47 +0000

    modified code

--- food_order_project/food_order_project/settings.py
+++ food_order_project/food_order_project/settings.py
@@ -83,12 +83,12 @@ WSGI_APPLICATION = 'food_order_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -138,3 +138,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = MEDIA_DIR
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
