4cd45686f662c91db9ba9703ceb6b1a8f51a1a19
Rohit Wadi <rohitwadi1@gmail.com>
2024-03-11 17:10:06 +0000

    modified code in script

--- food_order_project/build_files.sh
+++ food_order_project/build_files.sh
@@ -1,4 +1,7 @@
 echo "BUILD START"
+yum install sqlite-devel
+./configure
+make && make altinstall
 pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
