4323ed5a1671fe9243d18335c72d171e08795719
Rohit Wadi <rohitwadi1@gmail.com>
2024-03-13 16:54:26 +0000

    updated

--- food_order_project/build_files.sh
+++ food_order_project/build_files.sh
@@ -1,6 +1,5 @@
 echo "BUILD START"
 yum install sqlite-devel
-yum install python3.9
 pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
