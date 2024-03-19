4773f567eea2108454294b972f15ccdc8e465630
Rohit Wadi <rohitwadi1@gmail.com>
2024-03-19 16:47:30 +0000

    build file

--- food_order_project/build_files.sh
+++ food_order_project/build_files.sh
@@ -1,4 +1,6 @@
 echo "BUILD START"
+pkg install databases/py36-sqlite3
+pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
