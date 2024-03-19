3d56ce74d4fa8e936a8a567c66a19645ace143e0
Rohit Wadi <rohitwadi1@gmail.com>
2024-03-19 17:26:21 +0000

    modified change

--- food_order_project/build_files.sh
+++ food_order_project/build_files.sh
@@ -5,6 +5,6 @@ cd /usr/src && curl https://www.python.org/ftp/python/${PY39_VERSION}/Python-${P
 cd Python-${PY39_VERSION} && ./configure --enable-optimizations && make -j4 altinstall &&\
 rm -rf /usr/src/Python-${PY39_VERSION}
 pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+/usr/src/python3.9 -m pip install -r requirements.txt
+/usr/src/python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
