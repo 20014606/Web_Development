3af24b9bf92b3576e6e21f7383f58076fc1181f7
Rohit Wadi <rohitwadi1@gmail.com>
2024-03-19 16:51:48 +0000

    modified changes

--- food_order_project/build_files.sh
+++ food_order_project/build_files.sh
@@ -1,6 +1,10 @@
 echo "BUILD START"
-pkg install databases/py36-sqlite3
+RUN yum install -y gcc openssl-devel bzip2-devel libffi-devel make sqlite-devel
+ARG PY37_VERSION=3.7.6
+RUN cd /usr/src && curl https://www.python.org/ftp/python/${PY37_VERSION}/Python-${PY37_VERSION}.tgz | tar -xz &&\
+  cd Python-${PY37_VERSION} && ./configure --enable-optimizations && make -j4 altinstall &&\
+  rm -rf /usr/src/Python-${PY37_VERSION}
 pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3.7 -m pip install -r requirements.txt
+python3.7 manage.py collectstatic --noinput --clear
 echo "BUILD END"
