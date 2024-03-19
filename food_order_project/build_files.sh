26c81742d5c037285907dcd91cdc460d27d7be08
Rohit Wadi <rohitwadi1@gmail.com>
2024-03-19 17:31:28 +0000

    reverted change

--- food_order_project/build_files.sh
+++ food_order_project/build_files.sh
@@ -1,8 +1,5 @@
 echo "BUILD START"
 yum install -y gcc openssl-devel bzip2-devel libffi-devel make sqlite-devel
-PY39_VERSION=3.9
-cd /usr/src && curl https://www.python.org/ftp/python/${PY39_VERSION}/Python-${PY39_VERSION}.tgz | tar -xz &&\
-cd Python-${PY39_VERSION} && ./configure --enable-optimizations && make -j4 altinstall
 pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
