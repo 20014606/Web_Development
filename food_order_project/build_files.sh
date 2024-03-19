770a7fd32a9f7e5380429148d79256af1c203a13
Rohit Wadi <rohitwadi1@gmail.com>
2024-03-19 17:16:30 +0000

    modified changes to 3.9

--- food_order_project/build_files.sh
+++ food_order_project/build_files.sh
@@ -1,10 +1,10 @@
 echo "BUILD START"
 yum install -y gcc openssl-devel bzip2-devel libffi-devel make sqlite-devel
-PY37_VERSION=3.7.6
-cd /usr/src && curl https://www.python.org/ftp/python/${PY37_VERSION}/Python-${PY37_VERSION}.tgz | tar -xz &&\
-cd Python-${PY37_VERSION} && ./configure --enable-optimizations && make -j4 altinstall &&\
-rm -rf /usr/src/Python-${PY37_VERSION}
-pip3.7 install --upgrade pip
-python3.7 -m pip install -r requirements.txt
-python3.7 manage.py collectstatic --noinput --clear
+PY39_VERSION=3.9
+cd /usr/src && curl https://www.python.org/ftp/python/${PY39_VERSION}/Python-${PY39_VERSION}.tgz | tar -xz &&\
+cd Python-${PY39_VERSION} && ./configure --enable-optimizations && make -j4 altinstall &&\
+rm -rf /usr/src/Python-${PY39_VERSION}
+pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
