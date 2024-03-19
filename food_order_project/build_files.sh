5ac51b11b6786c15e2e4e45afaca386ce1cc9a9a
Rohit Wadi <rohitwadi1@gmail.com>
2024-03-19 16:32:39 +0000

    Steps changed

--- food_order_project/build_files.sh
+++ food_order_project/build_files.sh
@@ -1,7 +1,4 @@
 echo "BUILD START"
-git config --global core.ignorecase false
-yum install sqlite-devel
-pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
