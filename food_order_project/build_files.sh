74afc37c3dba8a0004d4daedad16a3e762e97b71
Rohit Wadi <rohitwadi1@gmail.com>
2024-03-19 17:06:12 +0000

    modified changes

--- food_order_project/build_files.sh
+++ food_order_project/build_files.sh
@@ -4,7 +4,7 @@ PY37_VERSION=3.7.6
 cd /usr/src && curl https://www.python.org/ftp/python/${PY37_VERSION}/Python-${PY37_VERSION}.tgz | tar -xz &&\
 cd Python-${PY37_VERSION} && ./configure --enable-optimizations && make -j4 altinstall &&\
 rm -rf /usr/src/Python-${PY37_VERSION}
-pip install --upgrade pip
+pip3.7 install --upgrade pip
 python3.7 -m pip install -r requirements.txt
 python3.7 manage.py collectstatic --noinput --clear
 echo "BUILD END"
