8639ce3d7282ef0132acf96f7ad9275dcb683f17
Rohit Wadi <rohitwadi1@gmail.com>
2024-03-20 11:01:43 +0000

    added new line of code

--- food_order_project/build_files.sh
+++ food_order_project/build_files.sh
@@ -11,6 +11,10 @@ python -m pip install --upgrade pip
 echo "Building the project..."
 python -m pip install -r requirements.txt
 
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
 # Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
