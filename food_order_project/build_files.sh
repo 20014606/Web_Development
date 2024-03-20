f16638f5c0057ff84b4521235c2a60e096b650a5
Rohit Wadi <rohitwadi1@gmail.com>
2024-03-20 10:36:44 +0000

    code changed .sh file

--- food_order_project/build_files.sh
+++
@@ -1,6 +0,0 @@
-echo "BUILD START"
-yum install -y gcc openssl-devel bzip2-devel libffi-devel make sqlite-devel
-pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo "BUILD END"
